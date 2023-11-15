18cf465a592a20a28857e570793a148e7007b916
Özcan Kara <ozcankara810@gmail.com>
2023-11-15 09:30:51 +0300

    some changes

--- firebase_app/src/firebase/index.js
+++ firebase_app/src/firebase/index.js
@@ -1,11 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCEjgEcOTR42tST4O9qt30rMbkXLi0Cr_A",
   authDomain: "fir-app-d33af.firebaseapp.com",
@@ -13,9 +10,7 @@ const firebaseConfig = {
   storageBucket: "fir-app-d33af.appspot.com",
   messagingSenderId: "900031742998",
   appId: "1:900031742998:web:8d8a95d8d9804b90a9a137",
-  measurementId: "G-LJZ1Z338V8"
+  measurementId: "G-LJZ1Z338V8",
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
--- firebase_app/src/pages/index.js
+++ firebase_app/src/pages/index.js
@@ -1,5 +1,23 @@
+import { db } from "@/firebase";
+import { collection, addDoc } from "firebase/firestore"; 
 export default function Home() {
+  const addData = async ()=>{
+try {
+  const docRef = await addDoc(collection(db, "users"), {
+    first: "Ozcan",
+    last: "Kara",
+    born: 2000
+  });
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
+  }
   return (
-    <div>Hello</div>
+    <div> 
+      <button onClick={addData}>
+        Add Data
+      </button>
+    </div>
   )
 }
